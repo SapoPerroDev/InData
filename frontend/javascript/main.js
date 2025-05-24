03285b052960ad6f7830db333ca251ec78e3ba1b
SapoPerroDev <bgutierrez@unimayor.edu.co>
2025-05-23 23:03:07 -0500

    refactor(API-frontend): cambio de función de obtención de madres

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -1,4 +1,26 @@
-fetch("http://127.0.0.1:8000/api/madres/")
+async function cargarMadres() {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/madres/");
+    if (!response.ok) throw new Error("Error al cargar los datos");
+
+    const data = await response.json();
+    const lista = document.getElementById("lista-madres");
+
+    data.forEach(madre => {
+      const li = document.createElement("li");
+      li.textContent = `${madre.p_nombre} - ${madre.s_nombre}`;
+      lista.appendChild(li);
+    });
+
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+cargarMadres();
+  
+  
+  /*fetch("http://127.0.0.1:8000/api/madres/")
     .then(res => res.json())
     .then(data => {
       const lista = document.getElementById("lista-madres");
@@ -8,4 +30,4 @@ fetch("http://127.0.0.1:8000/api/madres/")
         console.log(`${madre.nombre} - ${madre.telefono}`);
         lista.appendChild(li);
       });
-});
+    });*/
